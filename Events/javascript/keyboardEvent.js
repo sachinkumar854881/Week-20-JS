d07c404d2ec37b8caf556bca37dc01010a60338b
sachin <sachinkumar854881@gmail.com>
2024-01-18 09:28:30 +0530

    add keyboardevent.js

--- Events/javascript/keyboardEvent.js
+++ Events/javascript/keyboardEvent.js
@@ -1,7 +1,13 @@
-// keyboard events
-const input = document.getElementById("getName")
-console.log('input:',input)
-input.addEventListener('keydown',function(event){
+// program ko run karenge toh pahele keydown chalega phir keyup chalega
+// keydown
+const kd = document.getElementById("getName")
+console.log('input:',kd)
+kd.addEventListener('keydown',function(event){
     console.log('event.key:',event.key)
     console.log("key is pressed")
+})
+// keyup
+let ku = document.getElementById("getName")
+ku.addEventListener('keyup',function(event){
+    console.log(event.target,event.target.value)
 })
