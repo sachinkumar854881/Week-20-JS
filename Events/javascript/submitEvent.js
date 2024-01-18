b1b03bb141d5923ada5278700fe71bbfd266f243
sachin <sachinkumar854881@gmail.com>
2024-01-18 09:29:47 +0530

    add submitevent.js

--- Events/javascript/submitEvent.js
+++ Events/javascript/submitEvent.js
@@ -1,5 +1,17 @@
 // submit event
-document.getElementById('userForm').addEventListener('submit',function(e){
-    console.log('form Submitted')
+let getVal = document.getElementById('userForm')
+getVal.addEventListener('submit',function(e){
     e.preventDefault();
+    let uname = document.getElementById("getName")
+    let upass = document.getElementById("getpwd")
+    // console.log(uname.value)
+    // console.log(upass.value)
+    if (uname.value == "" || upass.value == "") {
+        console.log("Please fill UserName and Password !");
+      }else {
+        console.log("This form has been successfully submitted!");
+        console.log(`This form has a username of ${uname.value} and password of ${upass.value}`);
+        uname.value = "";
+        upass.value = "";
+      }
 })
