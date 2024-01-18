8e5542cebf894c90b30c92564ce088b5a521e0ff
sachin <sachinkumar854881@gmail.com>
2024-01-18 09:39:12 +0530

    add updated files

--- Events/javascript/submitEvent.js
+++ Events/javascript/submitEvent.js
@@ -4,14 +4,13 @@ getVal.addEventListener('submit',function(e){
     e.preventDefault();
     let uname = document.getElementById("getName")
     let upass = document.getElementById("getpwd")
-    // console.log(uname.value)
-    // console.log(upass.value)
+
     if (uname.value == "" || upass.value == "") {
         console.log("Please fill UserName and Password !");
-      }else {
+    }else{
         console.log("This form has been successfully submitted!");
         console.log(`This form has a username of ${uname.value} and password of ${upass.value}`);
         uname.value = "";
         upass.value = "";
-      }
+    }
 })
